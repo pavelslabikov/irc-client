a4613460c584fc5e385077db2c551d6578bea66d
pashka <59769714+pavelslabikov@users.noreply.github.com>
2020-09-20 16:26:46 +0500

    Добавлен файл с исключениями
    Обработка некорректного конфиг файла

---
+++ errors.py
@@ -0,0 +1,13 @@
+class ApiError(Exception):
+    arg = ""
+
+
+class ConfigError(ApiError):
+    def __init__(self, invalid_config):
+        for key, value in invalid_config.items():
+            self.arg += f"\n[{key}]\n"
+            for option_key, option_value in value.items():
+                self.arg += f"{option_key} = {option_value}\n"
+
+    def __str__(self):
+        return f"Некорректное содержимое файла config.ini: {self.arg}"
