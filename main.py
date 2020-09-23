d651e3c5d99a0a15eb47a425ba5122b56e3e8e24
pashka <59769714+pavelslabikov@users.noreply.github.com>
2020-09-23 15:54:35 +0500

    Добавил предварительную обработку конфига

--- main.py
+++ main.py
@@ -9,18 +9,19 @@ DEFAULT_CONFIG = {
 }
 
 
-def check_config(config: ConfigParser):
-    if not config.has_section("Settings") or not config.has_section("Servers"):
-        raise errors.ConfigError(config)
+def get_config() -> ConfigParser:
+    current_config = ConfigParser(allow_no_value=True)
+    if not os.path.exists("config.ini"):
+        with open("config.ini", "w") as file:
+            current_config.read_dict(DEFAULT_CONFIG)
+            current_config.write(file)
+
+    current_config.read("config.ini")
+    if not current_config.has_section("Settings") or not current_config.has_section("Servers"):
+        raise errors.ConfigError(current_config)
+    return current_config
 
 
 if __name__ == "__main__":
-    client_config = ConfigParser(allow_no_value=True)
-    if not os.path.exists("config.ini"):
-        with open("config.ini", "w") as file:
-            client_config.read_dict(DEFAULT_CONFIG)
-            client_config.write(file)
-    client_config.read("config.ini")
-    check_config(client_config)
-    client = Client(client_config)
+    client = Client(get_config())
     client.start_client()
